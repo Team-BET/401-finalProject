83244229a5b7cd7ed7c668750b0a8f37f6b55580
Eyob1984 <tamireyob@gmail.com>
2020-08-11 19:34:06 -0700

    Header and Footer components created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './components/header/';
+import Footer from './components/footer';
 
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
+    <Header />
+    <Footer />
     </div>
   );
 }
---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Header(props){
+  return(
+    <>
+    <p>BET Music Guessing Game</p>
+    </>
+  )
+}
+
+export default Header;
