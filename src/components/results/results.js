8af939704db2879fd86326a47ca12a0314ae8a70
Eyob1984 <tamireyob@gmail.com>
2020-08-18 21:30:02 -0700

    fix some bugs

--- src/components/results/results.js
+++ src/components/results/results.js
@@ -1,18 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link, Redirect } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from 'axios';
 
 const Results = (props) => {
+  const { isAuthenticated, user } = useAuth0();
   console.log("result", props);
+  console.log('props.myUser', user)
 
+
+
+ 
+  async function makeGetRequest () {
+    let res = await axios.get("https://bet-backend.herokuapp.com/api/v1/users")
+  }
+  makeGetRequest()
+  
   return (
+    isAuthenticated &&(
     <div>
       <p>Results: {props.correctAnswers - props.inCorrectAnswers}</p>
       <Link class="btn btn-danger btn-lg" to="/Songloading">
         Back To Game
       </Link>
     </div>
-  );
+  )
+  )
 };
 
 const mapStateToProps = (state) => {
